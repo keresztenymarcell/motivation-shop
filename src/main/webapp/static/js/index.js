98deb2ff30d2fe72a237fa8718992f231aa9de75
gergocsontos <gergo.csontos.123@gmail.com>
2021-11-08 14:04:03 +0100

    feat: refactor index.js, add IDs as values to selects

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,1 +1,10 @@
-console.warn("LOL");
+const main = {
+    init() {
+        const categories = document.getElementById("categories");
+        categories.addEventListener("mouseup", () => {
+            console.log("Reee");
+        })
+    }
+
+}
+main.init();
