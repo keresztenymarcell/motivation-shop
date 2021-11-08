ca4424ed95ccc96abbc5ebbb0394d1183a0b84e1
gergocsontos <gergo.csontos.123@gmail.com>
2021-11-08 16:22:26 +0100

    feat: add fetch to index.js, add gson to pom.xml, add ProductFilterApi servlet

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,9 +1,15 @@
 const main = {
     init() {
         const categories = document.getElementById("categories");
-        categories.addEventListener("mouseup", () => {
-            console.log("Reee");
+        categories.addEventListener("mouseup", async (event) => {
+            const categoryId = event.target.value;
+            const data = await main.fetchFromApi(`/api/filter?name=category&id=${categoryId}`);
+            console.log(data);
         })
+    },
+    async fetchFromApi(url) {
+        const response = await fetch(url);
+        return response.json();
     }
 
 }
