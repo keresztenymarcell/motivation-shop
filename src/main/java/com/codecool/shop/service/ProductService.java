71d8c64472d0937ecfc520e382c2a0d415a8472b
gergocsontos <gergo.csontos.123@gmail.com>
2021-11-08 10:38:56 +0100

    feat: add supplier methods to ProductService

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,18 +2,22 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 import java.util.List;
 
 public class ProductService{
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     public ProductCategory getProductCategory(int categoryId){
@@ -24,6 +28,16 @@ public class ProductService{
         var category = productCategoryDao.find(categoryId);
         return productDao.getBy(category);
     }
+
+    public Supplier getSupplier(int supplierId){
+        return supplierDao.find(supplierId);
+    }
+
+    public List<Product> getProductsForSupplier(int supplierId){
+        var supplier = supplierDao.find(supplierId);
+        return productDao.getBy(supplier);
+    }
+
 
 
 }
