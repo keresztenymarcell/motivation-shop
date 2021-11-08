74e0117349fdbc4fe2059b8f7e5a943b726eeab0
gergocsontos <gergo.csontos.123@gmail.com>
2021-11-08 17:41:18 +0100

    feat: add working filter by Category

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -3,14 +3,47 @@ const main = {
         const categories = document.getElementById("categories");
         categories.addEventListener("mouseup", async (event) => {
             const categoryId = event.target.value;
-            const data = await main.fetchFromApi(`/api/filter?name=category&id=${categoryId}`);
-            console.log(data);
+            const products = await main.fetchFromApi(`/api/filter?name=category&id=${categoryId}`);
+            main.clearProducts();
+            main.fillProductsWithProducts(products);
         })
     },
     async fetchFromApi(url) {
         const response = await fetch(url);
         return response.json();
-    }
+    },
+
+    fillProductsWithProducts(products) {
+        const productsDiv = document.getElementById("products");
+
+        for (let i = 0; i < products.length; i++) {
+            productsDiv.innerHTML += this.createCardFromProduct(products[i]);
+        }
+    },
+
+    createCardFromProduct(product) {
+        return `<div class="col col-sm-12 col-md-6 col-lg-4">
+    <div class="card">
+        <img class="" src=/static/img/product_${product.id}.jpg alt="" />
+        <div class="card-header">
+            <h4 class="card-title">${product.name}</h4>
+            <p class="card-text">${product.description}</p>
+        </div>
+        <div class="card-body">
+            <div class="card-text">
+                <p class="lead">100 USD???</p>
+            </div>
+            <div class="card-text">
+                <a class="btn btn-success" href="#">Add to cart</a>
+            </div>
+        </div>
+    </div>
+    </div>`
+    },
+
+    clearProducts() {
+        document.getElementById("products").textContent = "";
+    },
 
 }
 main.init();
