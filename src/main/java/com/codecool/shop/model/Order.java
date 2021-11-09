6b6cdf3a5dfa337bc79c47b06e82d0ec30567ffa
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-09 11:25:47 +0100

    feat: implement Order class

---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,45 @@
+package com.codecool.shop.model;
+
+
+import javax.sound.sampled.Line;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Order extends BaseModel{
+
+    private final Set<LineItem> cart = new HashSet<>();
+    private User user;
+
+    public Order(String name) {
+        super(name);
+    }
+
+    public void addItemToCart(LineItem item){
+        if(checkIfItemInCart(item)){
+            LineItem current = cart.stream().filter(x -> x.getName().equals(item.getName())).findFirst().get();
+            current.setQuantity(current.getQuantity() + 1);
+        }
+        else{
+            cart.add(item);
+        }
+    }
+
+    public void removeItemFromCart(LineItem item){
+
+        if(checkIfItemInCart(item)){
+            LineItem current = cart.stream().filter(x -> x.getName().equals(item.getName())).findFirst().get();
+            current.setQuantity(current.getQuantity() - 1);
+            if(current.getQuantity() == 0){
+                cart.remove(current);
+            }
+        }
+    }
+
+    private boolean checkIfItemInCart(LineItem item){
+        return cart.stream().anyMatch(lineItem -> lineItem.getName().equals(item.getName()));
+    }
+
+
+
+
+}
