0c6109759a5a653562c48f7f6e288ba4553a2b25
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-09 12:57:39 +0100

    feat: implement cartController servlet

---
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -0,0 +1,78 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.User;
+import com.codecool.shop.service.ProductService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "cartController", urlPatterns = {"/api/add-to-cart"}, loadOnStartup = 1)
+public class CartController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        UserDao userDataStore = UserDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore,supplierDataStore);
+
+
+        int id;
+
+        try {
+            id = Integer.parseInt(req.getParameter("id"));
+            if (id < 1) {
+                id = 1;
+            }
+        } catch (NumberFormatException e) {
+            id = 1;
+        }
+
+        Product product  = productDataStore.find(id);
+        LineItem lineItem = new LineItem(product);
+
+        User currentUser = userDataStore.find(1);
+        System.out.println(currentUser);
+
+        if(!currentUser.hasOrder()){
+            new Order(currentUser);
+        }
+        currentUser.getOrder().addItemToCart(lineItem);
+
+        System.out.println(currentUser.getOrder().getCart());
+
+
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print("{}");
+        out.flush();
+
+
+
+
+
+
+
+    }
+
+}
