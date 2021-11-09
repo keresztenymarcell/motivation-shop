e062eb06151120500054341f014cc8365c780c89
Betti Bodnar <bettinabodnar7@gmail.com>
2021-11-09 14:43:43 +0100

    feat: add extraFields to LineItem class.

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -3,30 +3,30 @@ package com.codecool.shop.model;
 import java.math.BigDecimal;
 
 public class LineItem extends BaseModel{
-
-
-
     private int quantity;
     private final BigDecimal productPrice;
+    private BigDecimal itemTotal;
+    private int productId;
 
 
     public LineItem(Product product) {
         super(product.getName(), product.getDescription());
         this.quantity = 1;
         this.productPrice = product.getDefaultPrice();
-
+        this.productId = product.getId();
+        this.itemTotal = product.getDefaultPrice();
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+        this.itemTotal = calculateTotal();
     }
 
     public int getQuantity() {
         return quantity;
     }
 
-
-    public BigDecimal calculateTotal(){
+    private BigDecimal calculateTotal(){
         return productPrice.multiply(new BigDecimal(quantity));
     }
 
