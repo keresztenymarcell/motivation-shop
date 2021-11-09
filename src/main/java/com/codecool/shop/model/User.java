07c61ab4bdb6fbed112442b06574f810e9527a9c
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-09 12:56:45 +0100

    feat: add getter/setter to User class

--- src/main/java/com/codecool/shop/model/User.java
+++ src/main/java/com/codecool/shop/model/User.java
@@ -4,11 +4,29 @@ public class User extends BaseModel{
 
     private String password;
     private String email;
+
+
+
+
     private Order order;
 
     public User(String name) {
         super(name);
     }
+
+    public boolean hasOrder(){
+        return order != null;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+
 
 
 
