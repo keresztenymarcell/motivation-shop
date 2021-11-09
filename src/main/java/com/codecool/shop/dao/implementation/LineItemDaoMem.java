64b72641e667e18f29577b68b3411d1b3241df64
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-09 10:55:30 +0100

    feat: implement LineItemDao class

---
+++ src/main/java/com/codecool/shop/dao/implementation/LineItemDaoMem.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.LineItemDao;
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
+public class LineItemDaoMem implements LineItemDao {
+
+    @Override
+    public void add(LineItem item) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<LineItem> getAll(int orderId) {
+        return null;
+    }
+}
