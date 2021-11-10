6e2bca806e661dcd98f80f792c9bf5f6438e2962
gergocsontos <gergo.csontos.123@gmail.com>
2021-11-10 12:40:56 +0100

    feat: add InputValidator and OrderInformationInputChecker classes for input validation and set Order with data in CheckoutController

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,6 +1,17 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.Order;
+import com.codecool.shop.service.Service;
+import com.codecool.shop.util.OrderInformationInputChecker;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -23,24 +34,35 @@ public class CheckoutController extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        UserDao userDaoStore = UserDaoMem.getInstance();
+        Service service = new Service(productDataStore,productCategoryDataStore,supplierDataStore, userDaoStore);
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        String name = req.getParameter("name"); // cant be null
-        String email = req.getParameter("email"); // check
-        String country = req.getParameter("country"); // cant be null
-        String zipcode = req.getParameter("zipcode"); // can be null
-        String address = req.getParameter("address"); // cant be null
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String country = req.getParameter("country");
+        String zipcode = req.getParameter("zipcode");
+        String address = req.getParameter("address");
 
-        boolean x = false;
-        if (name.equals("Gergő")) {
-            x = true;
-        }
+        String errorMessage = OrderInformationInputChecker.getErrorMessageForInputs(name, email, country, address);
 
-        if (!x) {
-            context.setVariable("message", "Bad job!");
+        if (errorMessage != null) {
+            context.setVariable("message", errorMessage);
             engine.process("product/checkout.html", context, resp.getWriter());
         } else {
+            Order currentOrder = service.getUser(1).getOrder();
+            currentOrder.setOrderName(name);
+            currentOrder.setEmail(email);
+            currentOrder.setCountry(country);
+            currentOrder.setZipcode(zipcode);
+            currentOrder.setAddress(address);
+
+            System.out.println(currentOrder);
             // here we can connect Betty's servlet with the payment
             resp.sendRedirect("/");
         }
