e6ce8fec4bbe935aa12a4c50db5ce10d60916b86
Betti Bodnar <bettinabodnar7@gmail.com>
2021-11-10 14:49:17 +0100

    feat: started to implement loader animation.

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -7,15 +7,47 @@ const pay = {
         const paypalBtn = document.getElementById('paypal-button');
         creditBtn.addEventListener("click", pay.creditHandler);
         paypalBtn.addEventListener("click", pay.payPalHandler);
+        const loaderButton = document.getElementById("loader");
+        loaderButton.addEventListener("click", pay.loaderAnimation);
 
     },
-    creditHandler() {
-        const cardHolder = document.getElementById('cardholder').value;
-        console.log(cardHolder);
+    async creditHandler() {
+        const CVV = document.getElementById('cvv').value;
+        const url = `/payment/credit?cvv=${CVV}`;
+        const isSuccess = await pay.fetchFromApi(url)
+        console.log(isSuccess);
+        if (isSuccess == "true"){
+            const paymentCheckUrl=`/payment`;
+            await pay.fetchFromApi(paymentCheckUrl);
+        }
+        else{
+            console.log("alert");
+        }
 
     },
 
-    payPalHandler() {
+    async payPalHandler() {
+        const payPalUser = document.getElementById('paypal-user').value;
+        const url = `/payment/paypal?user=${payPalUser}`;
+        const isSuccess = await pay.fetchFromApi(url)
+        console.log(isSuccess);
+        if (isSuccess == "true"){
+            const paymentCheckUrl=`/payment`;
+            await pay.fetchFromApi(paymentCheckUrl);
+        }
+        else{
+            console.log("alert");
+        }
+
+
+
+    },
+    async fetchFromApi(url) {
+        const response = await fetch(url);
+        return response.json();
+    },
+
+    loaderAnimation(){
 
     },
 }
