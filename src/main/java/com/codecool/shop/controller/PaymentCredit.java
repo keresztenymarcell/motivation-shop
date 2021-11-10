9f201794ae67370e9670ef99e8c9e57b3528537d
Betti Bodnar <bettinabodnar7@gmail.com>
2021-11-10 14:49:41 +0100

    feat: add paymentApi class.

---
+++ src/main/java/com/codecool/shop/controller/PaymentCredit.java
@@ -0,0 +1,65 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.model.User;
+import com.codecool.shop.service.Service;
+import com.google.gson.Gson;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+
+@WebServlet(name = "paymentCredit", urlPatterns = {"/payment/credit"}, loadOnStartup = 1)
+public class PaymentCredit extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        UserDao userDataStore = UserDaoMem.getInstance();
+        Service service = new Service(productDataStore,productCategoryDataStore,supplierDataStore, userDataStore);
+
+        User user = service.getUser(1);
+
+        String isSuccess;
+
+        String cvv = req.getParameter("cvv");
+        if(cvv.equals("666")){
+            isSuccess = "false";
+            user.getOrder().setPaymentMethod("credit");
+            user.getOrder().setSuccessPayment(false);
+            System.out.println("false");
+
+        }else{
+            isSuccess = "true";
+            user.getOrder().setPaymentMethod("credit");
+            user.getOrder().setSuccessPayment(true);
+            System.out.println("true");
+        }
+
+        String responseString = new Gson().toJson(isSuccess);
+
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print(responseString);
+        out.flush();
+    }
+}
---
+++ src/main/java/com/codecool/shop/controller/PaymentPayPal.java
@@ -0,0 +1,65 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.User;
+import com.codecool.shop.service.Service;
+import com.google.gson.Gson;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "paymentPayPal", urlPatterns = {"/payment/paypal"}, loadOnStartup = 1)
+public class PaymentPayPal extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        UserDao userDataStore = UserDaoMem.getInstance();
+        Service service = new Service(productDataStore,productCategoryDataStore,supplierDataStore, userDataStore);
+
+        User user = service.getUser(1);
+
+
+        String isSuccess;
+
+        String userName = req.getParameter("user");
+        if(userName.equals("evil")){
+            isSuccess = "false";
+            System.out.println("false");
+            user.getOrder().setPaymentMethod("paypal");
+            user.getOrder().setSuccessPayment(false);
+
+        }else{
+            isSuccess = "true";
+            System.out.println("true");
+            user.getOrder().setPaymentMethod("paypal");
+            user.getOrder().setSuccessPayment(true);
+        }
+
+        String responseString = new Gson().toJson(isSuccess);
+
+
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        out.print(responseString);
+        out.flush();
+    }
+}
