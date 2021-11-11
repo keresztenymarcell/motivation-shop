217d29fb4cc2c662eadc7cd60e36ebeeec473690
Gergő Csontos <gergo.csontos.123@gmail.com>
2021-11-11 23:42:57 +0100

    feat: add 2 pets and 2 plants

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -28,8 +28,6 @@ public class Initializer implements ServletContextListener {
         //setting up a new supplier
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
         supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
         Supplier ted = new Supplier("Ted", "TEDx is a program of local, self-organized events that bring people together to share a TED-like experience");
         supplierDataStore.add(ted);
         Supplier nike = new Supplier("Nike", "Sport brand");
@@ -42,10 +40,14 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(event);
         ProductCategory trade = new ProductCategory("Advert", "Video", "A motivating video.");
         productCategoryDataStore.add(trade);
-        ProductCategory elixir = new ProductCategory("Elixir", "Consumable", "A drink.");
+        ProductCategory elixir = new ProductCategory("Elixir", "Consumable", "An enchanting drink to enhance your powers.");
         productCategoryDataStore.add(elixir);
         ProductCategory poster = new ProductCategory("Poster", "Picture", "A motivating piece of paper.");
         productCategoryDataStore.add(poster);
+        ProductCategory pet = new ProductCategory("Pet", "Pet", "An actual animal that you need to care for.");
+        productCategoryDataStore.add(pet);
+        ProductCategory plant = new ProductCategory("Plant", "Plant", "A literal plant. Not as motivating as a pet, but still dies if you don't take care of it.");
+        productCategoryDataStore.add(plant);
 
 
         User testUser = new User("Teszt Tamás");
@@ -58,8 +60,7 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Bill Gates", new BigDecimal("12.500"), "USD", "The next outbreak? We are not ready.", event, ted));
         productDataStore.add(new Product("Just Do It rights", new BigDecimal("479"), "USD", "Dooooooooo it!.", trade, nike));
         productDataStore.add(new Product("Never give up!", new BigDecimal("100"), "USD", "I never lose. I either win or I learn.", trade, cola));
-        productDataStore.add(new Product("TestLa", new BigDecimal("9999"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", elixir, amazon));
-        productDataStore.add(new Product("Puppy Poster", new BigDecimal("100"), "USD", "A cute puppy to motivate you.", poster, amazon));
+        productDataStore.add(new Product("Puppy Poster", new BigDecimal("100"), "USD", "It's not real, but it's cute and motivates you.", poster, amazon));
 
 
         Supplier witchCraftBrewery = new Supplier("Witchcraft Brewery", "The most enchanting beverages from all the realms");
@@ -67,9 +68,9 @@ public class Initializer implements ServletContextListener {
         Supplier absolut = new Supplier("Absolut", "Original Sweden alcoholic products");
         supplierDataStore.add(absolut);
 
-        productDataStore.add(new Product("Potion of Happiness", new BigDecimal("666.69"), "USD", "The potion of choice for anyone feeling a bit under the weather.", elixir, absolut));
+        productDataStore.add(new Product("Potion of Happiness", new BigDecimal("30"), "USD", "The potion of choice for anyone feeling a bit under the weather.", elixir, absolut));
         productDataStore.add(new Product("Potion of Motivation", new BigDecimal("666.69"), "USD", "Do you feel tired after a long TW week? This beverage can fill you with Determination for your upcoming SI tasks like nothing else!", elixir, witchCraftBrewery));
-        productDataStore.add(new Product("Potion of Inspiration", new BigDecimal("666.69"), "USD", "Better than the kiss of any muse.", elixir, witchCraftBrewery));
+        productDataStore.add(new Product("Potion of Inspiration", new BigDecimal("999.99"), "USD", "Better than the kiss of any muse.", elixir, witchCraftBrewery));
         productDataStore.add(new Product("Potion of Endurance", new BigDecimal("399.99"), "USD", "The best potion to keep your muscles strong and your heart brave whenever you need some extra help.", elixir, witchCraftBrewery));
         productDataStore.add(new Product("Potion of Vigilance", new BigDecimal("449.99"), "USD", "To keep your eyes peeled even in the greatest of exhaustion.", elixir, witchCraftBrewery));
 
@@ -78,7 +79,10 @@ public class Initializer implements ServletContextListener {
         Supplier averagePlantShop = new Supplier("Average Plant Shop", "Not too meh plants for those with no patience for better creatures");
         supplierDataStore.add(averagePlantShop);
 
-
+        productDataStore.add(new Product("Cool Talking Parrot", new BigDecimal("100"), "USD", "It's a pretty cool parrot. You can teach it to swear at you if you don't study or sing the American anthem while you meditate.", pet, perfectPetShop));
+        productDataStore.add(new Product("Lazy Turtle", new BigDecimal("30"), "USD", "Pretty lazy. If you don't do your job, you'll end up like it.", pet, perfectPetShop));
+        productDataStore.add(new Product("Cactus", new BigDecimal("15"), "USD", "The perfect starter for responsible adults. If you manage to kill this one, there is no hope for you.", plant, averagePlantShop));
+        productDataStore.add(new Product("Philodendron", new BigDecimal("30"), "USD", "Green means good job. Brown means bad job.", plant, averagePlantShop));
 
 
 
