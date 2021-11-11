5aa0d5ffc40ff8e7a5adad9d4653bdd0c0096a15
Betti Bodnar <bettinabodnar7@gmail.com>
2021-11-11 13:23:24 +0100

    feat: fillOrderHeader with userData.

--- src/main/webapp/static/js/confirmation.js
+++ src/main/webapp/static/js/confirmation.js
@@ -17,7 +17,7 @@ const conf = {
         const confirm = document.getElementById("confirm");
         confirm.textContent = "";
         confirm.innerHTML += await conf.fillHeader(order);
-        confirm.innerHTML += conf.fillAllProduct(order.cart) + conf.fullSubTotal(order) + conf.fillFooter(order);
+        confirm.innerHTML += conf.fillAllProduct(order.cart) + conf.fullSubTotal(order) + conf.fillFooter();
 
 
     },
@@ -30,28 +30,28 @@ const conf = {
 
     },
 
-    async fillHeader(cartValue){
+    async fillHeader(order){
         return `<div class="row d-flex justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="text-left logo p-2 px-5"> <img src="" width="50"> </div>
                 <div class="invoice p-5">
-                    <h5>Your order Confirmed!</h5> <span class="font-weight-bold d-block mt-4">Hello, Chris</span> <span>You order has been confirmed and will be shipped in next two days!</span>
+                    <h5>Your order Confirmed!</h5> <span class="font-weight-bold d-block mt-4">Hello, ${order.orderName}</span> <span>You order has been confirmed and will be shipped in next two days!</span>
                     <div class="payment border-top mt-3 mb-3 border-bottom table-responsive">
                         <table class="table table-borderless">
                             <tbody>
                             <tr>
                                 <td>
-                                    <div class="py-2"> <span class="d-block text-muted">Order Date</span> <span>12 Jan,2018</span> </div>
+                                    <div class="py-2"> <span class="d-block text-muted">Order Date</span> <span>${order.orderTime}</span> </div>
                                 </td>
                                 <td>
-                                    <div class="py-2"> <span class="d-block text-muted">Order No</span> <span>MT12332345</span> </div>
+                                    <div class="py-2"> <span class="d-block text-muted">Order No</span> <span>${order.id}</span> </div>
                                 </td>
                                 <td>
-                                    <div class="py-2"> <span class="d-block text-muted">Payment</span> <span><img src="https://img.icons8.com/color/48/000000/mastercard.png" width="20" /></span> </div>
+                                    <div class="py-2"> <span class="d-block text-muted">Payment</span> <span>${order.paymentMethod}</span> </div>
                                 </td>
                                 <td>
-                                    <div class="py-2"> <span class="d-block text-muted">Shiping Address</span> <span>414 Advert Avenue, NY,USA</span> </div>
+                                    <div class="py-2"> <span class="d-block text-muted">Shiping Address</span> <span>${order.address}</span> </div>
                                 </td>
                             </tr>
                             </tbody>
@@ -79,7 +79,7 @@ const conf = {
                         <div class="product-qty"> <span class="d-block">Quantity:${product.quantity}</span> </div>
                     </td>
                     <td width="20%">
-                        <div class="text-right"> <span class="font-weight-bold">${product.itemTotal}</span> </div>
+                        <div class="text-right"> <span class="font-weight-bold">${product.itemTotal} USD</span> </div>
                     </td>
                 </tr>
                             `
@@ -108,7 +108,7 @@ const conf = {
                     </div>`
     },
 
-    fillFooter(productsInCart){
+    fillFooter(){
         return `<p>We will be sending shipping confirmation email when the item shipped successfully!</p>
                     <p class="font-weight-bold mb-0">Thanks for shopping with us!</p> <span>Motivation Team</span>
                 </div>
