e45d4bb27f35e8f6d7985670c2a4312e46cb5aa3
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-22 21:05:25 +0100

    file: add DatabaseManager class

---
+++ src/main/java/com/codecool/shop/dao/implementation/DatabaseManager.java
@@ -0,0 +1,41 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.*;
+import com.codecool.shop.model.ProductCategory;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    LineItemDao lineItemDao;
+    ProductCategoryDao productCategoryDao;
+    ProductDao productDao;
+    SupplierDao supplierDao;
+    UserDao userDao;
+
+    private void setup() throws SQLException{
+        DataSource dataSource = connect();
+        lineItemDao = new LineItemDaoJdbc(dataSource);
+        productCategoryDao = new ProductCategoryDaoJdbc(dataSource);
+        productDao = new ProductDaoJdbc(dataSource);
+        supplierDao = new SupplierDaoJdbc(dataSource);
+        userDao = new UserDaoJdbc(dataSource);
+
+    }
+
+
+    private DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String dbName = System.getenv("PSQL_DB_NAME");
+        String user = System.getenv("PSQL_USER_NAME");
+        String password = System.getenv("PSQL_PASSWORD");
+
+        System.out.println("Trying to connect....");
+        dataSource.getConnection().close();
+        System.out.println("Connection OK!");
+
+        return dataSource;
+    }
+}
