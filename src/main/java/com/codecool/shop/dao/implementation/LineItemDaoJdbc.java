4ad75d16bd6256a29c0fba6b3061c13a2ec1407b
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-22 21:05:58 +0100

    file: add LineItemDaoJdbc class

---
+++ src/main/java/com/codecool/shop/dao/implementation/LineItemDaoJdbc.java
@@ -0,0 +1,36 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.LineItemDao;
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.model.Product;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class LineItemDaoJdbc implements LineItemDao {
+
+    DataSource dataSource;
+
+    public LineItemDaoJdbc(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    @Override
+    public void add(LineItem item) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<LineItem> getAll(int orderId) {
+        return null;
+    }
+}
