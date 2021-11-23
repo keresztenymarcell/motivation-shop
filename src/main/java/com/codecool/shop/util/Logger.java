5082eea3013fc900a6f5127d40f89d22ba4c4802
Gergő Csontos <gergo.csontos.123@gmail.com>
2021-11-23 19:27:30 +0100

    add proper time + formatting

--- src/main/java/com/codecool/shop/util/Logger.java
+++ src/main/java/com/codecool/shop/util/Logger.java
@@ -1,5 +1,9 @@
 package com.codecool.shop.util;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -12,34 +16,56 @@ public class Logger {
     }
 
     public void info(String message) {
-
-        System.out.printf("%s [INFO] %s : %s\n", getDate(), className, message);
+        String log = String.format("%s [INFO] %s : %s\n", getDate(), className, message);
+        doStuff(log);
 
     }
 
     public void trace(String message) {
-
-        System.out.printf("%s [TRACE] %s : %s\n", getDate(), className, message);
+        String log = String.format("%s [TRACE] %s : %s\n", getDate(), className, message);
+        doStuff(log);
 
     }
 
     public void debug(String message) {
-
-        System.out.printf("%s [DEBUG] %s : %s\n", getDate(), className, message);
+        String log = String.format("%s [DEBUG] %s : %s\n", getDate(), className, message);
+        doStuff(log);
 
     }
 
     public void warn(String message) {
-        System.out.printf("%s [WARNING] %s : %s\n", getDate(), className, message);
+        String log = String.format("%s [WARNING] %s : %s\n", getDate(), className, message);
+        doStuff(log);
     }
 
     public void error(String message) {
-        System.out.printf("%s [ERROR] %s : %s\n", getDate(), className, message);
+        String log = String.format("%s [ERROR] %s : %s\n", getDate(), className, message);
+        doStuff(log);
+    }
+
+    private void doStuff(String log) {
+        System.out.print(log);
+        writeToFile(log);
     }
 
     private String getDate() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd hh:mm:ss");
         return now.format(formatter);
+    }
+
+    private void writeToFile(String log) {
+        try {
+            File file = new File("logs");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.append(log);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
