770712919cfcd4048be6eafcd178bd611fac687b
keresztenymarcell <kereszteny.marcell@gmail.com>
2021-11-23 19:34:13 +0100

    feat: implement add() to jdbc

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/LineItemDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/LineItemDaoJdbc.java
@@ -5,6 +5,7 @@ import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.Product;
 
 import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class LineItemDaoJdbc extends DatabaseConnection implements LineItemDao {
@@ -14,13 +15,27 @@ public class LineItemDaoJdbc extends DatabaseConnection implements LineItemDao {
     public LineItemDaoJdbc(DataSource dataSource){
         this.dataSource = dataSource;
     }
+
     @Override
     public void add(LineItem item) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO line_items(quantity, order_id, product_id) VALUES (?, ?, ?)";
+            PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setInt(1, item.getQuantity());
+            //how to get order id
+            st.setInt(2, 1);
+            st.setInt(3, item.getProductId());
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next();
+            item.setId(rs.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public Product find(int id) {
+    public LineItem find(int id) {
         return null;
     }
 
