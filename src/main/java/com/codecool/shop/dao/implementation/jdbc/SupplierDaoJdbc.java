a298c9fcf31957f3173d5606cec4e2f8ef44e95c
Betti Bodnar <bettinabodnar7@gmail.com>
2021-11-23 22:48:40 +0100

    feat: implement find and getALl methods in SupplierDaoJdbc.

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
@@ -5,6 +5,11 @@ import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierDaoJdbc extends DatabaseConnection implements SupplierDao {
@@ -24,7 +29,20 @@ public class SupplierDaoJdbc extends DatabaseConnection implements SupplierDao {
 
     @Override
     public Supplier find(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, description FROM suppliers WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                return null;
+            }
+            Supplier supplier = new Supplier(rs.getString(2), rs.getString(3));
+            supplier.setId(id);
+            return supplier;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading author with id: " + id, e);
+        }
     }
 
     @Override
@@ -34,6 +52,19 @@ public class SupplierDaoJdbc extends DatabaseConnection implements SupplierDao {
 
     @Override
     public List<Supplier> getAll() {
-        return null;
+        List<Supplier> result = new ArrayList<>();
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, description FROM suppliers";
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            while (rs.next()) {
+                Supplier supplier = new Supplier(rs.getString(2),
+                        rs.getString(3));
+                supplier.setId(rs.getInt(1));
+                result.add(supplier);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all products: " + e);
+        }
+        return result;
     }
 }
