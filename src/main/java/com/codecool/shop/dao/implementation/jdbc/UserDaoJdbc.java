1e10e3b4b7a622502fa85b5cfbdbd555bb4602af
Betti Bodnar <bettinabodnar7@gmail.com>
2021-11-23 22:48:57 +0100

    feat: implement find method in UserDaoJdbc.

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJdbc.java
@@ -4,6 +4,10 @@ import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.User;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoJdbc extends DatabaseConnection implements UserDao {
@@ -24,7 +28,20 @@ public class UserDaoJdbc extends DatabaseConnection implements UserDao {
 
     @Override
     public User find(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT email, name, password, order_id FROM users WHERE id=?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                return null;
+            }
+            User user = new User(rs.getString(2));
+            user.setId(id);
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading author with id: " + id, e);
+        }
     }
 
     @Override
