81e602ae706b6a6831d0482bf911f24444751ec4
gergocsontos <gergo.csontos.123@gmail.com>
2021-11-24 15:19:59 +0100

    feat: set up testing environment

---
+++ src/test/java/com/codecool/shop/service/ProductServiceTest.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void getUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSupplier() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductsForCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductsForSupplier() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllSuppliers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllCategories() {
+    }
+}
